c4ef894cb8aa2ed0ad64061b952cba6ef5a296c4
exploit-j <kkarismajh@gmail.com>
2022-05-12 14:57:15 +0900

    review 스타일작업

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 const Review = ({ item }) => {
+  let date = new Date(item.created_at).toLocaleString();
+
   return (
     <div className="review-container">
-      <p>{item.author}</p>
-      <p>{item.content}</p>
-      <p>{item.created_at}</p>
+      <p className="auth">{item.author}</p>
+      <p className="content">{item.content}</p>
+      <p className="created-date">{date}</p>
     </div>
   );
 };
